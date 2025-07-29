35e33a7c57dfcf2997dc7a2dc44ff303f01828b9
Sampath G.S.N <90971404+GSNSAMPATH@users.noreply.github.com>
2025-07-29 23:09:22 +0530

    Initial commit

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,22 +1,22 @@
-import { FC, HTMLProps } from 'react';
+import { FC } from 'react';
+
 interface ButtonProps {
-  color?: string; // e.g. 'yellow-300', 'blue-500', etc.
+  color?: string;
   size?: string;
-  size0?: string; // e.g. 'sm', 'md', 'lg', etc.
+  size0?: string; // Remove this line if you don't need it
   children: React.ReactNode;
-  className?: string; // Add this property to pass additional class names
 }
 
 const Button: FC<ButtonProps> = ({
-  color = 'yellow-400',
-  size = '4',
-  size0 = '2',
+  color = 'yellow-300',
+  size = 'md',
+  size0 = '5',
   children,
-  className,
   ...props
 }) => {
-const buttonClassName = `bg-${color} font-semibold py-${size} px-6 rounded-full shadow hover:bg-${color}-400 transition`;  return (
-    <button className={`${buttonClassName} ${className}`} {...props}>
+  const buttonClassName = `bg-${color} font-semibold py-${size} px-${size0} rounded-full shadow hover:bg-${color}-400 transition`; // Use size0 here if you need it
+  return (
+    <button className={buttonClassName} {...props}>
       {children}
     </button>
   );
