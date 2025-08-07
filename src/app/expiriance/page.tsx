0e1cbc69c93b2223f1e5d8af758c13cf3fb0cfae
Sampath G.S.N <90971404+GSNSAMPATH@users.noreply.github.com>
2025-08-07 23:32:21 +0530

    Updated footer component

---
+++ src/app/expiriance/page.tsx
@@ -0,0 +1,13 @@
+import ExperienceCollection from "@/components/expiriance/ExperienceCollection";
+import Experience from "@/components/expiriance/Expiriance";
+
+
+export default function Expiriance() {
+  return (
+    <>
+      {/* Other sections */}
+      <Experience />
+      <ExperienceCollection/>
+    </>
+  );
+}
