66f5acdb4d7256cab8f1b277628818e1bd0d090f
Nalina2001 <144620106+Nalina2001@users.noreply.github.com>
2025-08-13 18:42:20 +0530

    new

--- src/app/tara-garden-vila/page.tsx
+++ src/app/tara-garden-vila/page.tsx
@@ -1,6 +1,10 @@
 import Hero from "@/components/tara-garden-vila/hero";
 import VillaDescription from "@/components/tara-garden-vila/description";
 import FeaturesSection from "@/components/tara-garden-vila/featues";
+import Gallery from "@/components/tara-garden-vila/gallery-tara-garden";
+
+
+
 
 export default function Home() {
   return (
@@ -8,6 +12,7 @@ export default function Home() {
       <Hero />
       <VillaDescription />
       <FeaturesSection />
+      <Gallery />
     </>
   );
 }
