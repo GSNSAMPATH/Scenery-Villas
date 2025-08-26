548c36b1a65f1504fc499891b36889c56daddbbf
Sampath G.S.N <90971404+GSNSAMPATH@users.noreply.github.com>
2025-08-26 23:20:35 +0530

    updated in 2025.08.26

--- src/components/expiriance/ExperienceCollection.tsx
+++ src/components/expiriance/ExperienceCollection.tsx
@@ -5,46 +5,46 @@ import Image from "next/image";
 
 const experiences = [
   {
-    title: "Private Chef Dining",
-    description: "Enjoy delicious meals prepared by our in-house chef with authentic local flavors and seasonal ingredients .",
-    image: "https://res.cloudinary.com/diatamf9x/image/upload/v1754318415/WhatsApp_Image_2025-08-03_at_10.38.05_4c6eb6b6_alvpoz.jpg",
+    title: "Bentota Beach",
+    description: "Bentota Beach is a long, sandy beach with calm waters that are perfect for swimming and sunbathing. There are also a number of water sports operators offering activities such as surfing, paddleboarding, and kayaking. The beach is also home to a number of restaurants and bars, as well as a few hotels and resorts.",
+    image: "https://res.cloudinary.com/diatamf9x/image/upload/v1756225071/g1_kasqyz.webp",
   },
   {
-    title: "Luxury Spa Treatment",
-    description: "Relax and rejuvenate with our premium in-villa spa and massage services.",
-    image: "https://res.cloudinary.com/diatamf9x/image/upload/v1754320249/WhatsApp_Image_2025-08-03_at_10.38.04_c21be287_pjzltg.jpg",
+    title: "Aluthgama Beach",
+    description: "Aluthgama Beach is a bit smaller than Bentota Beach, but it is equally beautiful. The beach is known forits strong waves, which make it a popular spot for surfing. There are also a number of other water sportsavailable, such as paddleboarding and kayaking. The beach is also home to a number of restaurants andbars, as well as a few hotels and resorts.",
+    image: "https://res.cloudinary.com/diatamf9x/image/upload/v1756225071/g2_gwwost.webp",
   },
   {
-    title: "Outdoor Swimming Pool",
-    description: "Indoor and outdoor swimming pools for your relaxation and leisure.",
-    image: "https://res.cloudinary.com/diatamf9x/image/upload/v1754320249/WhatsApp_Image_2025-08-03_at_10.38.04_c21be287_pjzltg.jpg",
+    title: "Bentota Boat safari",
+    description: "Glide along the serene river, your vessel guided by experienced local guides who possess an intimateknowledge of the area's hidden treasures and secrets. As you venture deeper into this enchantingecosystem, prepare to encounter a captivating array of creatures that call this verdant sanctuary home.",
+    image: "https://res.cloudinary.com/diatamf9x/image/upload/v1756225072/g3_x7sdxy.webp",
   },
   {
-    title: "Gym and Yoga Studio",
-    description: "Stay fit and healthy with our state-of-the-art gym and yoga studio.",
-    image: "https://res.cloudinary.com/diatamf9x/image/upload/v1754318415/WhatsApp_Image_2025-08-03_at_10.38.05_4c6eb6b6_alvpoz.jpg",
+    title: "Turtle hatchery Kosgoda",
+    description: "Scattered along Sri Lanka's coast, turtle hatcheries serve as sanctuaries for endangered sea turtles,providing refuge, rehabilitation, and education. Visitors witness the delicate hatching process, hold tinyhatchlings, and release them back into the sea, fostering a connection to these remarkable creatures.",
+    image: "https://res.cloudinary.com/diatamf9x/image/upload/v1756225072/g4_oadpuu.webp",
   },
   {
-    title: "Spa and Massage",
-    description: "Relax and rejuvenate with our premium spa and massage services.",
-    image: "https://res.cloudinary.com/diatamf9x/image/upload/v1754320249/WhatsApp_Image_2025-08-03_at_10.38.04_c21be287_pjzltg.jpg",
+    title: "Bentota river water sports",
+    description: "Feel the surge of adrenaline as you carve through the glistening waters on a jet ski, the wind whippingthrough your hair and the riverbanks blurring past your vision. Hold on tight as you experience theexhilarating twists and turns of water skiing, the spray of water refreshing your face as you glide acrossthe surface. Embark on a thrilling ride on a banana boat, bouncing along the waves and sharing laughterwith friends as you navigate the river's bends.",
+    image: "https://res.cloudinary.com/diatamf9x/image/upload/v1756225072/g5_czhxe8.webp",
   },
   {
-    title: "Outdoor Swimming Pool",
-    description: "Indoor and outdoor swimming pools for your relaxation and leisure.",
-    image: "https://res.cloudinary.com/diatamf9x/image/upload/v1754320249/WhatsApp_Image_2025-08-03_at_10.38.04_c21be287_pjzltg.jpg",
+    title: "Bentota river fishing",
+    description: "The Bentota River, winding its way through the lush landscapes of Sri Lanka, offers an angler's paradise,a haven where the thrill of catching a prized fish meets the serenity of nature's embrace. As you castyour line into the shimmering waters, anticipation builds, the gentle tug of the river promising arewarding encounter with the diverse aquatic life that thrives within its depths.",
+    image: "https://res.cloudinary.com/diatamf9x/image/upload/v1756225071/g6_wuwvlu.webp",
   }
 ];
 
 
 export default function ExperienceCollection() {
   return (
-    <div className="bg-[var(--background0)] py-16 px-6 ">
+    <div className="bg-[var(--background0)] py-10 px-6 ">
       {/* Heading */}
 
 
       {/* Cards */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-20 mx-4 sm:mx-50 mb-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-10  mx-auto md:max-w-7xl mb-10">
         {experiences.map((item, index) => (
           <div
             key={index}
@@ -61,7 +61,7 @@ export default function ExperienceCollection() {
             />
             <div
               className="absolute inset-0 bg-black/40 
-                opacity-100 sm:opacity-0 sm:group-hover:opacity-100 
+                opacity-100 sm:opacity-100 sm:group-hover:opacity-100
                 transition-opacity duration-300 p-5 flex flex-col justify-end"
             >
               <h3 className="text-white text-xl font-bold mb-2">{item.title}</h3>
--- src/components/expiriance/Expiriance.tsx
+++ src/components/expiriance/Expiriance.tsx
@@ -32,18 +32,18 @@ export default function Experience() {
   }
 
   return (
-    <section className="bg-[var(--background0)] py-30 px-4 text-center">
-      <h2 className={`${lobster.className} text-4xl font-bold text-green-900 leading-tight mb-4 md:mb-8 md:mt-20`}>Experience</h2>
+    <section className="bg-[var(--background0)] py-10 px-4 text-center">
+      <h2 className={`${lobster.className} text-4xl font-bold text-green-900 leading-tight mb-4 md:mb-8 md:mt-30 mt-20`}>Experience</h2>
       {/* "text-5xl font-bold mb-8 mt-20" */}
-      <p className="text-lg sm:text-xl text-black max-w-3xl mx-auto mb-8">
+      <p className="text-lg sm:text-xl text-black max-w-7xl mx-auto mb-0">
         Experience is Scenery Villas in Bentota, Sri Lanka, offering
         family-friendly and group accommodations with private spaces and baby
         pools, perfect for relaxing getaways.
       </p>
 
-      <Button color="black" size="3" onClick={handleClick}>
+      {/* <Button color="black" size="3" onClick={handleClick}>
         <span className="text-white">Book Now</span>
-      </Button>
+      </Button> */}
     </section>
   );
 }
